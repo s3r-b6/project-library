762a9662212573ffcac15298ad5e8095b33df002
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-05-25 10:33:31 +0200

    bugFix y limpieza de código
    ya funciona con números de más de un dígito; código limpiado y comentado; falta estilar bien el css

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ window.onload = () => {
     myLibrary.push(newBook);
   }
 
-  //placeholders
+  //test
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", false);
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
+  addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
   addBook("J.R.R. Tolkien", "The Hobbit", "399", false);
   addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
   addBook("J.R.R. Tolkien", "The Hobbit", "399", true);
@@ -57,13 +65,13 @@ window.onload = () => {
     showBooks();
   }
 
-  //posibles funcionalidades:
   //@mode: cantidad de libros por estantería
   function showBooks() {
     let n;
+    //función que añade los libros
     for (i in myLibrary) {
       let currentBooks = parseInt(i);
-      //genera estanterías (divs) para organizar los libros de cuatro en cuatro
+      //genera estanterías (divs) para organizar los libros
       if (currentBooks % 4 == 0) {
         n = currentBooks / 4;
         document.querySelector("#showcase").innerHTML += `
@@ -89,49 +97,42 @@ window.onload = () => {
       `;
     }
 
-    //toma el segundo valor del array (la clase n${i} y toma después sólo el i (el índice dentro del array myLibrary))
+    //toma de la classlist la id (n10 p.ej., y borra la n): índice del array de libros.
     document.querySelectorAll("#removeButton").forEach((el) => {
       el.addEventListener("click", function (e) {
-        //console.log(e.target.parentNode.classList);
-        console.log(n);
-        let parentNode = e.target.parentNode.parentNode.classList[1][1];
-        //console.log(parentNode);
+        let parentNode = e.target.parentNode.parentNode.classList[1];
+        parentNode = parentNode.slice(1);
         delete myLibrary[parentNode];
-        //console.log(myLibrary);
         restartShowcase();
       });
     });
     document.querySelectorAll("#markAsRead").forEach((el) => {
       el.onclick = function (e) {
-        let parentNode = e.target.parentNode.parentNode.classList[1][1];
+        let parentNode = e.target.parentNode.parentNode.classList[1];
+        parentNode = parentNode.slice(1);
         myLibrary[parentNode].read = !myLibrary[parentNode].read == true;
         restartShowcase();
       };
     });
 
+    //esta función encuentra dónde se ha activado el evento y añade la clase que gira el libro
     document.querySelectorAll(".book").forEach((el) => {
       el.addEventListener("click", function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-
         let n =
           e.target.parentNode.parentNode.classList[1] ||
           e.target.parentNode.classList[1] ||
           e.target.classList[1];
-        n = n[1];
-        console.log("n: " + n);
-
-        if (document.querySelector(`.n${n}`).classList.contains("rotateSide")) {
-          document.querySelector(`.n${n}`).classList.remove("rotateSide");
-          document.querySelector(`.n${n}`).classList.add("rotateFront");
-        } else if (
-          !document.querySelector(`.n${n}`).classList.contains("rotateSide")
-        ) {
-          document.querySelector(`.n${n}`).classList.add("rotateSide");
-          if (
-            document.querySelector(`.n${n}`).classList.contains("rotateFront")
-          ) {
-            document.querySelector(`.n${n}`).classList.remove("rotateFront");
+        n = n.slice(1);
+        if (document.querySelector(`.n${n}`)) {
+          let getClasses = document.querySelector(`.n${n}`).classList;
+          if (getClasses.contains("rotateSide")) {
+            getClasses.remove("rotateSide");
+            getClasses.add("rotateFront");
+          } else if (!getClasses.contains("rotateSide")) {
+            getClasses.add("rotateSide");
+            if (getClasses.contains("rotateFront")) {
+              getClasses.remove("rotateFront");
+            }
           }
         }
       });
